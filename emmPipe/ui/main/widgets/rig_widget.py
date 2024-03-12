a0b508494e1f1f01ca71cc9aab42f697dab1536a
Einar <einarmarmagnuss@gmail.com>
2024-03-11 23:46:26 -0700

    Add Osseous py file and  button to RigWidget

--- emmPipe/ui/main/widgets/rig_widget.py
+++ emmPipe/ui/main/widgets/rig_widget.py
@@ -3,6 +3,10 @@ import os
 from PySide2 import QtWidgets
 from PySide2.QtWidgets import QVBoxLayout
 
+import maya.cmds as cmds
+
+from emmPipe.ui.osseous.osseousUI import OsseousUI
+
 class RigWidget(QtWidgets.QWidget):
     
         def __init__(self, c_data, parent=None):
@@ -17,15 +21,29 @@ class RigWidget(QtWidgets.QWidget):
         
         def add_widgets(self):
             
+            self.osseous_button = QtWidgets.QPushButton('Osseous')
             self.button = QtWidgets.QPushButton('Build')
 
         def add_layouts(self):
             main_layout = QVBoxLayout(self)
             
+            main_layout.addWidget(self.osseous_button)
             main_layout.addWidget(self.button)
         
         def add_connections(self):
             self.button.clicked.connect(self.c_data.print_build_script_path)
 
+            self.osseous_button.clicked.connect(self.show_osseous_ui)
+
         def update_build_script_path(self):
             self.c_data.build_scripts_path = os.path.join(self.c_data.asset, 'python', 'rigBuild', 'build.py')
+
+        def show_osseous_ui(self):
+
+            if os.getenv('EMMPIPE_MODE') == 'Development':
+                if cmds.workspaceControl(OsseousUI.get_workspace_control_name(), q=True, exists=True):
+                    cmds.deleteUI(OsseousUI.get_workspace_control_name())
+                
+                print(f'{OsseousUI.get_workspace_control_name()} Deleted')
+            
+            OsseousUI.display()
